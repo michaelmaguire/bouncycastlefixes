200d5a4a337df66bf3acf6831852a08c7d563bdd
michael.maguire <michael.maguire@27f88ba2-6e31-11de-84e2-6792fbbe797f>
2009-07-14 12:07:49 +0000

    #572 BouncyCastle does not use buffering for outgoing data.
    Added buffering for TLS output stream. Each write will accumulate the data and only encode and write it out on a flush command. 
    This significantly reduces the amount of data sent for outgoing mail.
    
    
    {{{
    C:\temp\bwmorg2944>svn info
    Path: .
    URL: https://svn1.hosted-projects.com/bluewhalesystems/mail/trunk/Client/Java/J2SECore/src/org
    Repository Root: https://svn1.hosted-projects.com/bluewhalesystems/mail
    Repository UUID: b9c5b17c-2d79-024f-b1b2-a718191054b9
    Revision: 2944
    Node Kind: directory
    Schedule: normal
    Last Changed Author: Tatiana.Rybak
    Last Changed Rev: 2944
    Last Changed Date: 2007-07-02 15:15:37 +0100 (Mon, 02 Jul 2007)
    }}}

--- lcrypto-j2me/src/org/bouncycastle/crypto/tls/TlsOuputStream.java
+++ lcrypto-j2me/src/org/bouncycastle/crypto/tls/TlsOuputStream.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.crypto.tls;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -8,38 +9,109 @@ import java.io.OutputStream;
  */
 public class TlsOuputStream extends OutputStream
 {
-
+    /**
+     * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+     * 
+     * Added buffering to TLSOutputStream
+     */
+    private static final int   GROW_SIZE = 256;
+    private static final int   NEW_SIZE  = 1024;    
+    private byte[]             bufferedData;
+    private int                size;
+    private int                position;
+    
     private TlsProtocolHandler handler;
 
-    protected TlsOuputStream(TlsProtocolHandler handler)
+    protected TlsOuputStream( TlsProtocolHandler handler )
     {
         this.handler = handler;
+        
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+         * 
+         * Added buffering to TLSOutputStream
+         */        
+        size = GROW_SIZE;
+        position = 0;
+        this.bufferedData = new byte[NEW_SIZE];
     }
 
-    public void write(byte buf[], int offset, int len) throws IOException
+    public void write( byte buf[], int offset, int len ) throws IOException
     {
-        this.handler.writeData(buf, offset, len);
-    }
+        // DO NOT write the data immediately! TLS will encode the data and send it right away.
+        // For 1 byte of data it would send 37 bytes. Instead, accumulate until there is a flush call.
+        // Original code:
+        // this.handler.writeData(buf, offset, len);
 
-    public void write(int arg0) throws IOException
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+         * 
+         * Added buffering to TLSOutputStream
+         */
+        // check that we have enough room to write out the data
+        int available = size - position;
+        if( available < len )
+        {
+            int growBy = Math.max( len, GROW_SIZE );
+            byte[] newArray = new byte[size + growBy];
+            System.arraycopy( bufferedData, 0, newArray, 0, position );
+            bufferedData = newArray;
+            size += growBy;
+        }
+
+        System.arraycopy( buf, 0, bufferedData, position, len );
+        position += len;
+
+    }
+    
+    public void write( int arg0 ) throws IOException
     {
-        byte[] buf = new byte[1];
-        buf[0] = (byte)arg0;
-        this.write(buf, 0, 1);
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+         * 
+         * Added buffering to TLSOutputStream
+         */
+        // if there is no more room, grow the buffer 
+        if( size == position )
+        {
+            byte[] newArray = new byte[size + GROW_SIZE];
+            System.arraycopy( bufferedData, 0, newArray, 0, position );
+            bufferedData = newArray;
+            size += GROW_SIZE;
+        }
+        bufferedData[position] = (byte) arg0;
+        position++;
+        
+        // Original code:
+        //byte[] buf = new byte[1];
+        //buf[0] = (byte) arg0;
+        //this.write( buf, 0, 1 );
     }
 
-    /**  
-     * BlueWhaleSystems fix: Tatiana Rybak - 25 June 2007  
-     *   
-     * Fixed typo cose to be close  
-     */  
-    public void close() throws IOException  
+    /**
+     * BlueWhaleSystems fix: Tatiana Rybak - 25 June 2007
+     * 
+     * Fixed typo cose to be close.
+     */
+    public void close() throws IOException
     {
         handler.close();
     }
 
     public void flush() throws IOException
-    {
+    {       
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+         * 
+         * Added buffering to TLSOutputStream
+         */
+        // write out the data we have accumulated so far
+        this.handler.writeData( bufferedData, 0, position );
         handler.flush();
+        
+        // reset the buffer
+        size = NEW_SIZE;
+        position = 0;
+        this.bufferedData = new byte[size];        
     }
 }
