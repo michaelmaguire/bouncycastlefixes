26d5f29cdd165d3de0de425cc44403b23d6d2576
tatiana.rybak <tatiana.rybak@27f88ba2-6e31-11de-84e2-6792fbbe797f>
2010-03-16 11:45:42 +0000

    #572 BouncyCastle does not use buffering for outgoing data.
    Added buffering for TLS output stream. Each write will accumulate the data and
    only encode and write it out on a flush command.
    This significantly reduces the amount of data sent for outgoing mail.
    
    
    {{{
    C:\temp\bwmorg2944>svn info
    Path: .
    URL: https://svn1.hosted-
    projects.com/bluewhalesystems/mail/trunk/Client/Java/J2SECore/src/org
    Repository Root: https://svn1.hosted-projects.com/bluewhalesystems/mail
    Repository UUID: b9c5b17c-2d79-024f-b1b2-a718191054b9
    Revision: 2944
    Node Kind: directory
    Schedule: normal
    Last Changed Author: Tatiana.Rybak
    Last Changed Rev: 2944
    Last Changed Date: 2007-07-02 15:15:37 +0100 (Mon, 02 Jul 2007)
    }}}

--- trunk/lcrypto-j2me-145/src/bwmorg/bouncycastle/crypto/tls/TlsOuputStream.java
+++ trunk/lcrypto-j2me-145/src/bwmorg/bouncycastle/crypto/tls/TlsOuputStream.java
@@ -1,30 +1,111 @@
 package bwmorg.bouncycastle.crypto.tls;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * An OutputStream for an TLS connection.
  */
 public class TlsOuputStream extends OutputStream
 {
-    private byte[] buf = new byte[1];
+    /**
+     * BlueWhaleSystems fix: Tatiana Rybak - 02 July 2007
+     * 
+     * Added buffering to TLSOutputStream
+     */
+    private static final int   DEFAULT_BUFFER_SIZE = 32768; // 32KB
+
+    private final byte[]       bufferedData;
+    private int                position;
+
     private TlsProtocolHandler handler;
 
-    TlsOuputStream(TlsProtocolHandler handler)
+    /**
+     * BlueWhaleSystems fix: Tatiana Rybak - 07 Jan 2009
+     *  
+     * For testing only.
+     *  
+     **/
+    public TlsOuputStream( TlsProtocolHandler handler, int size )
     {
         this.handler = handler;
+
+        position = 0;
+        bufferedData = new byte[size];
     }
 
-    public void write(byte buf[], int offset, int len) throws IOException
+    protected TlsOuputStream( TlsProtocolHandler handler )
     {
-        this.handler.writeData(buf, offset, len);
+        this.handler = handler;
+
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 07 Jan 2009
+         */
+        position = 0;
+        bufferedData = new byte[DEFAULT_BUFFER_SIZE];
     }
 
-    public void write(int arg0) throws IOException
+    public void write( byte buf[], int offset, int len ) throws IOException
     {
-        buf[0] = (byte)arg0;
-        this.write(buf, 0, 1);
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 07 Jan 2009
+         * 
+         * 
+         * The original code simply wrote the data out without any buffering in place. In the extreme case of
+         * data length being 1 (a single byte being written out) the actual amount of data sent was 38 bytes,
+         * i.e., there were 37 bytes added as a wrapper by the TLS logic.
+         *
+         * tickets: 572 BouncyCastle does not use buffering for outgoing data.
+         * ticket:  2815 Client attachments: Attempting to send a message with an added attachment freezes 
+         * UI for a long time (and doesn't appear to have sent anything).
+         *
+         * So, instead we have a 32 kb buffer that is written out conditionally. If the amount of data passed
+         * in to this function exceeds the buffer size it is all written out immediately. If it fits in the
+         * buffer is it cached until the next flush (or possibly write) operation.
+         */
+
+        // Original code:
+        // this.handler.writeData(buf, offset, len);
+        int available = bufferedData.length - position;
+        if( available < len )
+        {
+            flush();
+
+            if( bufferedData.length < len )
+            {
+                handler.writeData( buf, offset, len );
+            }
+            else
+            {
+                System.arraycopy( buf, offset, bufferedData, position, len );
+                position += len;
+            }
+        }
+        else
+        {
+            System.arraycopy( buf, offset, bufferedData, position, len );
+            position += len;
+        }
+    }
+
+    public void write( int arg0 ) throws IOException
+    {
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 07 Jan 2009
+         * 
+         * See comments above in void write( byte buf[], int offset, int len ) throws IOException
+         */
+
+        if( bufferedData.length == position )
+        {
+            flush();
+        }
+
+        bufferedData[position] = (byte) arg0;
+        position++;
+
+        // Original code:
+        //buf[0] = (byte)arg0;
+        //this.write(buf, 0, 1);
     }
 
     /** @deprecated Use 'close' instead */
@@ -35,11 +116,30 @@ public class TlsOuputStream extends OutputStream
 
     public void close() throws IOException
     {
-        handler.close();
+        /**
+         * BlueWhaleSystems fix: Michael Maguire - 10 Aug 2007
+         *
+         * Make sure we null out on close.
+         */
+        try
+        {
+            handler.close();
+        }
+        finally
+        {
+            handler = null;
+        }
     }
 
     public void flush() throws IOException
     {
+        /**
+         * BlueWhaleSystems fix: Tatiana Rybak - 07 Jan 2009
+         * 
+         * See comments above in void write( byte buf[], int offset, int len ) throws IOException
+         */
+        handler.writeData( bufferedData, 0, position );
         handler.flush();
+        position = 0;
     }
 }
